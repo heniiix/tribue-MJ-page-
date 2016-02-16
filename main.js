d1864786d49d329e675c8be9c352603e24bee659
Hugo ANTHOFER <hugoanthofer@hotmail.fr>
2016-02-16 13:03:30 +1100

    Create main.js

---
+++ main.js
@@ -0,0 +1,30 @@
+$(document).ready(function(){
+  $("body").addClass("bio");
+  $("#home-link").click(function(){
+    $("body").removeClass().addClass("bio");
+  });
+  $("#album1").click(function(){
+    $("body").removeClass().addClass("gottobethere");
+  });
+  $("#album2").click(function(){
+    $("body").removeClass().addClass("ben");
+  });
+  $("#album3").click(function(){
+    $("body").removeClass().addClass("musicandme");
+  });
+  $("#album4").click(function(){
+    $("body").removeClass().addClass("forever");
+  });
+  $("#album5").click(function(){
+    $("body").removeClass().addClass("off");
+  });
+  $("#album6").click(function(){
+    $("body").removeClass().addClass();
+  });
+  $("#album7").click(function(){
+    $("body").removeClass().addClass();
+  });
+});
+                
+               
+
